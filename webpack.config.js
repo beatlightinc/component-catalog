ca491c3ab3dd24f6817a82576df78c9283fc1dd6
Kevin Belanger <kevinlbelanger@gmail.com>
2018-03-05 14:25:47 -0500

    edit webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const jsEntryPath = path.resolve(__dirname, 'index.js');
+const jsEntryPath = path.resolve(__dirname, 'index.ts');
 const sassEntryPath = path.resolve(__dirname, 'scss', 'style.scss');
 const outputPath = path.resolve(__dirname, 'static');
 
@@ -17,36 +17,35 @@ const babelLoaderRule = {
 };
 
 module.exports = {
-    entry: "./src/index.ts",
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
-    },
+  entry: [
+    jsEntryPath,
+    sassEntryPath
+  ],
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+  output: {
+    path: outputPath,
+    filename: 'js/bundle.js'
+  },
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
+  module: {
+    rules: [
+      sassLoaderRule,
+      babelLoaderRule
+    ]
+  },
 
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+  plugins: [
+    new ExtractTextPlugin('css/style.css')
+  ],
 
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
-    },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
 
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
+  stats: {
+    assets: true,
+    modules: false
+  },
+
+  devtool: 'cheap-module-source-map'
 };
