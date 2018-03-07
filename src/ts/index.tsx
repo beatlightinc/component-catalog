55bae0ef839486e4ecfe452792a5f3cbe105d12e
Kevin Belanger <kevinlbelanger@gmail.com>
2018-03-07 16:11:41 -0500

    Don't need these yet

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk';
 import App from './app';
 
 ReactDOM.render(
