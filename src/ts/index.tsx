c20382223e40ebdbed102277caf3272cc6d100b6
Kevin Belanger <kevinlbelanger@gmail.com>
2018-03-07 14:19:26 -0500

    edit index.tsx to take in our app

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -1,2 +1,11 @@
-const str: string = 'any string, whatever';
-console.log(str);
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+import App from './app';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
