169048260a24b1c135d184dd7ac9cc7fc2c04749
Kevin Belanger <kevinlbelanger@gmail.com>
2018-03-07 15:52:03 -0500

    run webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -57,6 +57,7 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'static'),
     compress: true,
+    historyApiFallback: true,
     host: '0.0.0.0',
     port: 3000,
     stats: {
