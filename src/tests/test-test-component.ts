3e54a2de9b63d483ef129c797f1e4eb3184520c7
Kevin Belanger <kevinlbelanger@gmail.com>
2018-03-08 12:06:49 -0500

    get jsx test helpers

---
+++ src/tests/test-test-component.ts
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import test from 'ava';
+import { shallow } from 'enzyme';
+import { renderJSX, JSX } from 'jsx-test-helpers';
