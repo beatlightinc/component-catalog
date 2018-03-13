ebeea2bc717498286b1425977e0b5e2e7df54817
Kevin Belanger <kevinlbelanger@gmail.com>
2018-03-13 11:15:02 -0400

    Change package name, restructure project directories

--- viewer-app/App.tsx
+++ viewer-app/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import createBrowserHistory from 'history/createBrowserHistory';
 import { Router, Route, Switch } from 'react-router-dom';
-import Root from './views/root';
+import Root from './root';
 
 class App extends React.Component<{}, {}> {
   public render() {
