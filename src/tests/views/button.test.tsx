1077c42eb176a4da860c0ed96865ce8ba851ef85
Kevin Belanger <kevinlbelanger@gmail.com>
2018-03-14 12:58:11 -0400

    finish tests for button

--- src/tests/views/button.test.tsx
+++ src/tests/views/button.test.tsx
@@ -3,9 +3,12 @@ import * as ReactDOM from 'react-dom';
 import test from 'ava';
 import * as sinon from 'sinon';
 import { shallow } from 'enzyme';
-import Button from '../../ts/views/button';
+import Button from '../../ts/views/buttons/button';
 
 test('button onclick and text work if passed in', (t) => {
   const onClickSpy = sinon.spy();
-  const wrapper = shallow(<Button text="" onClick={onClickSpy}/>);
+  const wrapper = shallow(<Button buttonText="something" onClick={onClickSpy}/>);
+  wrapper.simulate('click');
+  t.is(onClickSpy.called, true);
+  t.is(wrapper.text(), 'something');
 });
