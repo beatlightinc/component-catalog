e6830658cb71759aa20eea238225f38bd43302ec
Kevin Belanger <kevinlbelanger@gmail.com>
2018-03-15 12:57:50 -0400

    Write a test to check if the onChange handler gets triggered

--- src/tests/views/inputs/input.test.tsx
+++ src/tests/views/inputs/input.test.tsx
@@ -7,5 +7,13 @@ import Input from '../../../ts/views/inputs/input';
 import Button from '../../../ts/views/buttons/button';
 
 test('<Input /> gets props passed in properly', (t) => {
-  const wrapper = shallow(<Input />);
+  const handleChange = sinon.spy();
+  const wrapper = shallow(<Input
+    inputType="text"
+    autoFocus={false}
+    inputValue=""
+    onHandleChange={handleChange}
+    disabled={false}/>);
+  wrapper.find('input').simulate('change', {target: {value: 'My new value'}});
+  t.is(handleChange.called, true);
 });
