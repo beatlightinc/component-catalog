f53298c641f65569b406fb56c8e4773adb115f44
Kevin Belanger <kevinlbelanger@gmail.com>
2018-03-16 14:37:16 -0400

    Add jsdom

--- src/setup-enzyme-adapter.js
+++ src/setup-enzyme-adapter.js
@@ -1,4 +1,6 @@
 const Adapter = require('enzyme-adapter-react-16');
 const { configure } = require('enzyme');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 configure({ adapter: new Adapter() });
