dc1087c51d6171d517c96927ae708783a322b99f
Kevin Belanger <kevinlbelanger@gmail.com>
2018-03-20 12:20:19 -0400

    more in the render function to render page range

--- src/ts/views/widgets/pagination.tsx
+++ src/ts/views/widgets/pagination.tsx
@@ -1,15 +1,43 @@
 import * as React from 'react';
 import PageButton from '../buttons/page-button';
+import * as classnames from 'classnames';
 
 const Pagination = (props: {
   activePage: number,
   totalPages: number,
   pageChanged?: (pageNumber: number) => void
 }) => {
-    const { activePage, totalPages, pageChanged } = props;
+    const { activePage, totalPages, pageChanged } = props
+    const breakElement = <div className="pagination-break">{'...'}</div>;
     let allPageElements = [];
     let pageRangeArr = [];
     let pageRange;
+
+    for (let i = 0; i < totalPages; i++) {
+      const isActive = i === activePage;
+      const pageClass = classnames('pagination-element', {
+        '(isActive)': isActive
+      });
+      allPageElements.push(
+        <PageButton onClick={pageChanged.bind(this, i)} buttonNumber={i} className={pageClass} />
+      );
+    }
+
+    if (totalPages <= 5) {
+      pageRangeArr = allPageElements;
+      pageRange = <div className="pagination-element-wrapper">{pageRangeArr}</div>;
+    }
+
+    if (activePage + 1 === 1) {
+      pageRangeArr = allPageElements.slice(0, activePage + 3);
+      pageRange = <div>
+        {pageRangeArr}
+        {breakElement}
+        {allPageElements[totalPages - 1]}
+      </div>;
+    }
+
+
     return (<div></div>);
 };
 
