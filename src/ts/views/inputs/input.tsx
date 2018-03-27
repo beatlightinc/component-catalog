948505c79e830baee82e04cbd5215fa422e228c8
Kevin Belanger <kevinlbelanger@gmail.com>
2018-03-27 15:48:15 -0400

    Some more styling and some more logic

--- src/ts/views/inputs/input.tsx
+++ src/ts/views/inputs/input.tsx
@@ -4,6 +4,9 @@ import * as classnames from 'classnames';
 const Input = (props: {
   inputType?: string,
   autoFocus?: boolean,
+
+  // Will eventually have to figure out icon logic
+  iconType?: string,
   onSubmission?: () => void
   onHandleChange: () => {},
   validationFunction?: () => boolean,
@@ -14,6 +17,7 @@ const Input = (props: {
   pattern?: string,
   placeholder?: string
 }) => {
+
   const {
     inputType,
     autoFocus,
@@ -28,13 +32,22 @@ const Input = (props: {
     value
   } = props;
 
-  const isValid = validationFunction ? validationFunction() : true;
+  let isValid = null;
+  if (validationFunction) {
+    isValid = validationFunction;
+  }
+
   const inputContainerClass = classnames('input-container', wrapperClassName, {
-    disabled: props.disabled
+    disabled: props.disabled,
+    valid: isValid && value.length > 0,
+    invalid: !isValid && value.length > 0
   });
   const inputClass = classnames('input-component', className, {
     disabled: props.disabled
   });
+
+  // TODO: Icon logic and implementation
+  const inputIcon = <div>{'ICON'}</div>;
 
   return (
       <div className={inputContainerClass}>
@@ -46,6 +59,7 @@ const Input = (props: {
           pattern={pattern}
           value={value}
           placeholder={placeholder ? placeholder : null}/>
+        {inputType ? inputIcon : null}
       </div>
   );
 };
