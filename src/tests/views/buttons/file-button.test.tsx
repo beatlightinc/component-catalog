67046d7cea0bba65a0d9ef8c56640fa3148a4456
Kevin Belanger <kevinlbelanger@gmail.com>
2018-04-05 15:44:04 -0400

    Write out first test to make sure the component works properly, then write cases for disabled

--- src/tests/views/buttons/file-button.test.tsx
+++ src/tests/views/buttons/file-button.test.tsx
@@ -5,6 +5,21 @@ import * as sinon from 'sinon';
 import { shallow } from 'enzyme';
 import FileButton from '../../../ts/views/buttons/file-button';
 
+test('<FileButton/> functions work properly', (t) => {
+  const onClickSpy = sinon.spy();
+  const onChangeSpy = sinon.spy();
+  const wrapper = shallow(
+    <FileButton
+      onClick={onClickSpy}
+      onChange={onChangeSpy}>
+      {'test upload'}
+    </FileButton>
+  );
+  wrapper.simulate('click');
+  t.is(onClickSpy.called, true);
+  t.is(onChangeSpy.called, true);
+});
+
 test('<FileButton /> gets proper classnames if disabled', (t) => {
   const wrapper = shallow(
     <FileButton disabled={true}>
@@ -14,5 +29,13 @@ test('<FileButton /> gets proper classnames if disabled', (t) => {
 });
 
 test('<FileButton /> functions do not get called if disabled', (t) => {
+  const onChangeSpy = sinon.spy();
+  const onClickSpy = sinon.spy();
+  const wrapper = shallow(
+    <FileButton
+      onChange={onChangeSpy}
+      onClick={onClickSpy}
+      disabled={true}/>
+  );
 
 });
