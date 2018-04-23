a4292123013e6eaa1ead805379bac797ff461b1d
Kevin Belanger <kevinlbelanger@gmail.com>
2018-04-23 14:17:34 -0400

    Create a type file to handle JSON so we can import and use it (finally)

--- src/ts/views/widgets/icon.tsx
+++ src/ts/views/widgets/icon.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as classnames from 'classnames';
-import IconLoader from '../../lib/icons/icon-loader';
 import { assign } from 'lodash';
+import * as ICONS from '../../icons/icon-constants.json';
 
 const Icon = (props: {
   name: string,
@@ -24,25 +24,6 @@ const Icon = (props: {
   } = props;
 
   const iconPath = `ic-${size}-${name}`;
-  IconLoader.load(iconPath).then((svg: any) => {
-    const wrapperElement = this.refs.wrapper.value;
-    wrapperElement.innerHTML = '';
-    wrapperElement.appendChild(svg);
-    wrapperElement.querySelector('svg *').getAttribute('fill', color);
-
-    if (size === 'mob') {
-      wrapperElement.querySelector('svg').getAttribute('width', 25);
-      wrapperElement.querySelector('svg').getAttribute('height', 25);
-    }
-
-    // Do the hover dance
-    if (hoverColor) {
-      const onHover = () => wrapperElement.querySelector('svg *').getAttribute('fill', hoverColor);
-      const offHover = () => wrapperElement.querySelector('svg *').getAttribute('fill', color);
-      // This might need mouseover instead of hover, trying to get rid of jQuery is hard work! :)
-      wrapperElement.parentNode.hover(onHover, offHover);
-    }
-  });
 
   let pixelSize;
   switch (size) {
