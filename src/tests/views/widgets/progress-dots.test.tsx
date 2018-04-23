e59680ddbb0643564d32ddad5c3cf6f8d25cc8e7
Mike <mikevespi@gmail.com>
2018-04-23 10:44:44 -0400

    progressDots test

---
+++ src/tests/views/widgets/progress-dots.test.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import test from 'ava';
+import * as sinon from 'sinon';
+import { shallow } from 'enzyme';
+import ProgressDots from '../../../ts/views/widgets/progress-dots';
+
+const onClickSpy = sinon.spy();
+
+test('ProgressDots get correct number of dots, as well as correct active step', (t) => {
+  const wrapper = shallow(<ProgressDots totalSteps={3} currentStep={2} stepOnClick={onClickSpy}/>);
+  t.is(wrapper.hasClass('progress-dots-wrapper'), true);
+  t.is(wrapper.find('.dot').length, 3);
+  t.is(wrapper.find('.clickable').length, 2);
+  t.is(wrapper.find('.current').length, 1);
+});
+
+test('ProgressDots maintains only one current step after being clicked', (t) => {
+  const wrapper = shallow(<ProgressDots totalSteps={3} currentStep={2} stepOnClick={onClickSpy}/>);
+  wrapper.find('.dot').first().simulate('click');
+  t.is(onClickSpy.calledWith(0), true);
+  t.is(wrapper.find('.clickable').length, 2);
+  t.is(wrapper.find('.current').length, 1);
+});
