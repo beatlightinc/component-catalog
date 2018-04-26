af6f51e298a2a00febbf8d060652d18e1c6449f8
Kevin Belanger <kevinlbelanger@gmail.com>
2018-04-26 13:18:11 -0400

    Build out basic switch, import react-switch library!

---
+++ src/ts/views/widgets/switch.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as classnames from 'classnames';
+
+const Switch = (props: {
+  onClick?: () => void,
+  active?: boolean,
+  color?: string
+}) => {
+  return (
+    <div></div>
+  );
+};
