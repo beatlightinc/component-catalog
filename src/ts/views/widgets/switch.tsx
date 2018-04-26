061a4519582290487b318fed92c3b4c94a47f608
Kevin Belanger <kevinlbelanger@gmail.com>
2018-04-26 14:37:32 -0400

    Some setup for the switch, I figure we may want a wrapper so we're just gunna extend the react-switch because it's cool

--- src/ts/views/widgets/switch.tsx
+++ src/ts/views/widgets/switch.tsx
@@ -1,12 +1,41 @@
 import * as React from 'react';
 import * as classnames from 'classnames';
+import Switch from 'react-switch';
 
-const Switch = (props: {
+const ToggleSwitch = (props: {
   onClick?: () => void,
   active?: boolean,
-  color?: string
+  disabled?: boolean,
+  color?: string,
+  children?: any,
+  className?: string
 }) => {
+
+  const {
+    onClick,
+    active,
+    disabled,
+    color,
+    children,
+    className
+  } = props;
+
+  const wrapperClass = classnames('toggle-switch-wrapper', className);
+
   return (
-    <div></div>
+    <div className={wrapperClass}>
+      <Switch
+        checked={props.active}
+        onChange={props.onClick}
+        disabled={disabled}
+        onColor={color ? color : '#0076FF'}
+        offColor={'#E5E5E5'}
+        handleDiameter={16}
+        height={16}
+        width={32}
+      />
+    </div>
   );
 };
+
+export default ToggleSwitch;
