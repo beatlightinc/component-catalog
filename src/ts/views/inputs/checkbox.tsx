9a7d66b720fd126f5351171a2138995a48bb21b4
Kevin Belanger <kevinlbelanger@gmail.com>
2018-04-27 11:51:34 -0400

    [wip] Checkbox

---
+++ src/ts/views/inputs/checkbox.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import PageButton from '../buttons/page-button';
+import * as classnames from 'classnames';
+
+const Checkbox = (props: {
+
+}) => {
+  return (
+      <div></div>
+  );
+};
+
+export default Checkbox;
