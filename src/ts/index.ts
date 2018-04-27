0c1bc26ce18356308fa4cf6b464ed4387daef468
Kevin Belanger <kevinlbelanger@gmail.com>
2018-04-27 11:59:21 -0400

    Clean up our export file by grouping the components together, otherwise it's a mess
    imports & exports as well

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,17 @@
+// Buttons
 export { default as Button } from './views/buttons/button';
 export { default as FileButton } from './views/buttons/file-button';
+
+// Labels
 export { default as Label } from './views/labels/label';
+
+// Inputs
 export { default as Input } from './views/inputs/input';
+export { default as Textarea } from './views/inputs/textarea';
+export { default as Checkbox } from './views/inputs/checkbox';
+
+// Widgets
 export { default as Pagination } from './views/widgets/pagination';
 export { default as AudioPlayer } from './views/widgets/audio-player';
-export { default as Textarea } from './views/inputs/textarea';
 export { default as ProgressDots } from './views/widgets/progress-dots';
 export { default as ProgressTabs } from './views/widgets/progress-tabs';
