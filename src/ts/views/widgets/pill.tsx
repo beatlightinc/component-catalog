60fa9800f2becc0dbb387682ba8e75f4d5716a4b
Kevin Belanger <kevinlbelanger@gmail.com>
2018-05-04 15:57:14 -0400

    Spacing wasn't working with button, might work better with a div so we can specify display options more? going to try out flex

--- src/ts/views/widgets/pill.tsx
+++ src/ts/views/widgets/pill.tsx
@@ -14,9 +14,9 @@ const Pill = (props: {
   const pillWrapperClass = classnames('pill', type, className, { removeable, active });
 
   return (
-    <button className={pillWrapperClass}>
+    <div className={pillWrapperClass}>
       <span className="pill-title">{children}</span>
-    </button>
+    </div>
   );
 };
 
