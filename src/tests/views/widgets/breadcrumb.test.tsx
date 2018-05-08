86b5868f08028a9a26cec901e7c004685e3db0a8
alexander-daniel <alex.vados@gmail.com>
2018-05-08 15:51:29 -0400

    add a test

---
+++ src/tests/views/widgets/breadcrumb.test.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as sinon from 'sinon';
+import test from 'ava';
+import { shallow } from 'enzyme';
+import BreadCrumbs from '../../../ts/views/widgets/breadcrumbs';
+
+test('<Pagination /> will render right amount of buttons for <= 5 total pages ', (t) => {
+  const path = [
+    'First Level',
+    'Second Level',
+    'Third Level'
+  ];
+
+  const onClickSpy = sinon.spy();
+
+  const elementWrapper = shallow(
+    <BreadCrumbs path={path} onClick={onClickSpy}/>
+  );
+
+  const crumbs = elementWrapper.find('.breadcrumb');
+
+  // The number of crumb elements should equal the path length we passed in.
+  t.is(crumbs.length, path.length);
+
+  // Click the first one and make sure the onClick handler does it's thing
+  elementWrapper.find('.label').first().simulate('click');
+
+  t.is(onClickSpy.calledWith(0), true);
+  t.pass();
+});
