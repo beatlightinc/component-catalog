784a7f9d3d699b1dcf77b3674eadf6874a5b1abd
Kevin Belanger <kevinlbelanger@gmail.com>
2018-05-08 15:24:54 -0400

    Write small test for classnames, can't think of what else to test (as the rest is just inline styled)

---
+++ src/tests/views/widgets/image.test.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import test from 'ava';
+import { shallow } from 'enzyme';
+import Image from '../../../ts/views/widgets/image';
+
+test('Image gets classnames properly and img recieves ', (t) => {
+  const wrapper = shallow(<Image url={`http://via.placeholder.com/350x150`} className="test-image"/>);
+  t.is(wrapper.hasClass('image-component test-image'), true);
+});
