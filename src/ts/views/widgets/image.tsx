e043f549f1ca1c6aa3b0910e5885104efc78a393
Kevin Belanger <kevinlbelanger@gmail.com>
2018-05-08 10:24:29 -0400

    [wip] Image component

---
+++ src/ts/views/widgets/image.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import * as classnames from 'classnames';
+
+const Image = (props: {
+  className?: string,
+  style?: any,
+  url: string
+}) => {
+  const {
+    className,
+    style,
+    url
+  } = props;
+
+  const imageClass = classnames('image-component', className);
+  const customStyle = style || {};
+  let backgroundStyle = {};
+
+  if (url !== null) {
+    backgroundStyle = { backgroundImage: `url(${url})` };
+  }
+
+  return (
+    <div
+      className={imageClass}
+      style={Object.assign(customStyle, backgroundStyle)}
+      >
+    </div>
+  );
+};
