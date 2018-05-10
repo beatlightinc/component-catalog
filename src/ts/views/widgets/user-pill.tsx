19427a4f0734a5ffbc36fb8f8187d90e06ff9182
Kevin Belanger <kevinlbelanger@gmail.com>
2018-05-10 15:33:00 -0400

    Modify user pill

--- src/ts/views/widgets/user-pill.tsx
+++ src/ts/views/widgets/user-pill.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as classnames from 'classnames';
 import Pill from './pill';
+import Avatar from './avatar';
 
 const UserPill = (props: {
   type?: string
@@ -11,7 +12,7 @@ const UserPill = (props: {
   const { type, userName, imgSrc, removeable } = props;
   return (
     <Pill type={type} removeable={removeable}>
-      {/* Separately styled avatar will go here. */}
+      <Avatar wrapperClass="user-pill-avatar" imgSrc={imgSrc} height={32} width={32} circle={true}/>
       {userName}
     </Pill>
   );
