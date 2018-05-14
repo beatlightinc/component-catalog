433aa4aefb07174724422e36ac83991d6d8da7ac
Kevin Belanger <kevinlbelanger@gmail.com>
2018-05-14 14:38:36 -0400

    nit display flex helper

--- viewer-app/sections/forms-viewer.tsx
+++ viewer-app/sections/forms-viewer.tsx
@@ -8,47 +8,52 @@ class FormsView extends React.Component<{}, {}> {
         <div className="viewer-app-content-wrapper">
           <div className="viewer-app-section-container">
             <h3>{'Text inputs'}</h3>
-            <div className="viewer-app-section-content">
+            <div className="+flex-column">
               <h4>{'Round'}</h4>
             </div>
-            <div className="viewer-app-section-content">
+            <div className="+flex-column">
               <h4>{'Default'}</h4>
             </div>
-            <div className="viewer-app-section-content">
+            <div className="+flex-column">
               <h4>{'Default with label'}</h4>
             </div>
           </div>
 
           <div className="viewer-app-section-container">
             <h3>{'Text Area'}</h3>
-            <div className="viewer-app-section-content">
+            <div className="+flex-column">
               <h4>{'Default'}</h4>
             </div>
           </div>
 
           <div className="viewer-app-section-container">
             <h3>{'States'}</h3>
-            <div className="viewer-app-section-content">
+            <div className="+flex-column">
               <h4>{'Default'}</h4>
             </div>
-            <div className="viewer-app-section-content">
+            <div className="+flex-column">
               <h4>{'Hover'}</h4>
             </div>
-            <div className="viewer-app-section-content">
+            <div className="+flex-column">
               <h4>{'Filled'}</h4>
             </div>
-            <div className="viewer-app-section-content">
+            <div className="+flex-column">
               <h4>{'Active'}</h4>
             </div>
-            <div className="viewer-app-section-content">
+            <div className="+flex-column">
               <h4>{'Disabled'}</h4>
             </div>
-            <div className="viewer-app-section-content">
+            <div className="+flex-column">
               <h4>{'Success'}</h4>
             </div>
-            <div className="viewer-app-section-content">
+            <div className="+flex-column">
               <h4>{'Error'}</h4>
             </div>
+          </div>
+
+          <div className="viewer-app-section-container">
+            <h3>{'Checkboxes'}</h3>
+            <div></div>
           </div>
         </div>
       </div>
