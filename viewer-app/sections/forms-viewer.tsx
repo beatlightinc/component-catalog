4d35f0c53b7b8d3516bde0682ecd1858260894ef
Kevin Belanger <kevinlbelanger@gmail.com>
2018-05-14 14:27:41 -0400

    more structure for forms page

--- viewer-app/sections/forms-viewer.tsx
+++ viewer-app/sections/forms-viewer.tsx
@@ -32,7 +32,34 @@ class FormsView extends React.Component<{}, {}> {
 
           <div className="viewer-app-section-container">
             <h3>{'States'}</h3>
-            <div className="viewer-app-section-content"></div>
+            <div className="viewer-app-section-content">
+              <h4>{'Default'}</h4>
+              <div className="component-box"></div>
+            </div>
+            <div className="viewer-app-section-content">
+              <h4>{'Hover'}</h4>
+              <div className="component-box"></div>
+            </div>
+            <div className="viewer-app-section-content">
+              <h4>{'Filled'}</h4>
+              <div className="component-box"></div>
+            </div>
+            <div className="viewer-app-section-content">
+              <h4>{'Active'}</h4>
+              <div className="component-box"></div>
+            </div>
+            <div className="viewer-app-section-content">
+              <h4>{'Disabled'}</h4>
+              <div className="component-box"></div>
+            </div>
+            <div className="viewer-app-section-content">
+              <h4>{'Success'}</h4>
+              <div className="component-box"></div>
+            </div>
+            <div className="viewer-app-section-content">
+              <h4>{'Error'}</h4>
+              <div className="component-box"></div>
+            </div>
           </div>
         </div>
       </div>
