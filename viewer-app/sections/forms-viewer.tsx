39a1195dca4be8256eb03223a86926f49b31eb09
Kevin Belanger <kevinlbelanger@gmail.com>
2018-05-14 14:22:57 -0400

    Get html structure that will carry over to other pages

--- viewer-app/sections/forms-viewer.tsx
+++ viewer-app/sections/forms-viewer.tsx
@@ -3,8 +3,38 @@ import * as React from 'react';
 class FormsView extends React.Component<{}, {}> {
   public render() {
     return (
-      <div className="viewer-app-section">
-        <div className="page-title">{'Forms'}</div>
+      <div className="viewer-app-page">
+        <div className="viewer-app-page-title">{'Forms'}</div>
+        <div className="viewer-app-content-wrapper">
+          <div className="viewer-app-section-container">
+            <h3>{'Text inputs'}</h3>
+            <div className="viewer-app-section-content">
+              <h4>{'Round'}</h4>
+              <div className="component-box"></div>
+            </div>
+            <div className="viewer-app-section-content">
+              <h4>{'Default'}</h4>
+              <div className="component-box"></div>
+            </div>
+            <div className="viewer-app-section-content">
+              <h4>{'Default with label'}</h4>
+              <div className="component-box"></div>
+            </div>
+          </div>
+
+          <div className="viewer-app-section-container">
+            <h3>{'Text Area'}</h3>
+            <div className="viewer-app-section-content">
+              <h4>{'Default'}</h4>
+              <div className="component-box"></div>
+            </div>
+          </div>
+
+          <div className="viewer-app-section-container">
+            <h3>{'States'}</h3>
+            <div className="viewer-app-section-content"></div>
+          </div>
+        </div>
       </div>
     );
   }
