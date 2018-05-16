e48c48cdb193adc578b58a7545563fbe0d8d5e93
Kevin Belanger <kevinlbelanger@gmail.com>
2018-05-16 15:36:27 -0400

    default buttons

--- viewer-app/sections/buttons-viewer.tsx
+++ viewer-app/sections/buttons-viewer.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Button } from '../../src/ts/index';
 
 class ButtonsView extends React.Component<{}, {}> {
   public render() {
@@ -9,14 +10,49 @@ class ButtonsView extends React.Component<{}, {}> {
         <div className="+display-flex">
           <div className="+flex-column">
             <h3>{'Standard'}</h3>
+
             <h4>{'Default'}</h4>
-            <div className="+display-flex"></div>
+            <div className="+display-flex">
+              <Button disabled={true}>{'Click'}</Button>
+              <Button color="grey" disabled={true}>{'Click'}</Button>
+              <Button color="white" disabled={true}>{'Click'}</Button>
+            </div>
+
             <h4>{'Medium'}</h4>
-            <div className="+display-flex"></div>
+            <div className="+display-flex">
+              <Button size="medium" disabled={true}>{'Click'}</Button>
+              <Button size="medium" color="grey" disabled={true}>{'Click'}</Button>
+              <Button size="medium" color="white" disabled={true}>{'Click'}</Button>
+            </div>
+
             <h4>{'Default with Icon'}</h4>
-            <div className="+display-flex"></div>
+            <div className="+display-flex">
+              <Button disabled={false}>
+                {'Click'}
+                <span className="icon-arrow-down" />
+              </Button>
+              <Button color="grey" disabled={false}>
+                {'Click'}
+                <span className="icon-arrow-down" />
+              </Button>
+              <Button color="white" disabled={false}>
+                {'Click'}
+                <span className="icon-arrow-down" />
+              </Button>
+            </div>
+
             <h4>{'Icon'}</h4>
-            <div className="+display-flex"></div>
+            <div className="+display-flex">
+              <Button disabled={false}>
+                <span className="icon-paperclip" />
+              </Button>
+              <Button color="grey" disabled={false}>
+                <span className="icon-paperclip" />
+              </Button>
+              <Button color="white" disabled={false}>
+                <span className="icon-paperclip" />
+              </Button>
+            </div>
           </div>
 
           <div className="+flex-column">
