d8295ca3a8656cca62106576d38165d1905b7bdf
Kevin Belanger <kevinlbelanger@gmail.com>
2018-05-17 14:15:47 -0400

    nits and utils

--- viewer-app/sections/buttons-viewer.tsx
+++ viewer-app/sections/buttons-viewer.tsx
@@ -9,24 +9,24 @@ class ButtonsView extends React.Component<{}, {}> {
 
         <div className="+display-flex">
           <div className="+flex-column">
-            <h3>{'Standard'}</h3>
+            <h3 className="+push-half-bottom +black-text +font-weight-bold">{'Standard'}</h3>
 
-            <h4>{'Default'}</h4>
-            <div className="+display-flex">
+            <h4 className="+push-half-bottom +grey-text +font-weight-bold">{'Default'}</h4>
+            <div className="+display-flex +push-half-bottom">
               <Button disabled={false}>{'Click'}</Button>
               <Button color="grey" disabled={false}>{'Click'}</Button>
               <Button color="white" disabled={false}>{'Click'}</Button>
             </div>
 
-            <h4>{'Medium'}</h4>
-            <div className="+display-flex">
+            <h4 className="+push-half-bottom +grey-text +font-weight-bold">{'Medium'}</h4>
+            <div className="+display-flex +push-half-bottom">
               <Button size="medium" disabled={false}>{'Click'}</Button>
               <Button size="medium" color="grey" disabled={false}>{'Click'}</Button>
               <Button size="medium" color="white" disabled={false}>{'Click'}</Button>
             </div>
 
-            <h4>{'Default with Icon'}</h4>
-            <div className="+display-flex">
+            <h4 className="+push-half-bottom +grey-text +font-weight-bold">{'Default with Icon'}</h4>
+            <div className="+display-flex +push-half-bottom">
               <Button disabled={false}>
                 {'Click'}
                 <span className="icon-arrow-down" />
@@ -41,8 +41,8 @@ class ButtonsView extends React.Component<{}, {}> {
               </Button>
             </div>
 
-            <h4>{'Icon'}</h4>
-            <div className="+display-flex">
+            <h4 className="+push-half-bottom +grey-text +font-weight-bold">{'Icon'}</h4>
+            <div className="+display-flex +push-half-bottom">
               <Button disabled={false}>
                 <span className="icon-paperclip" />
               </Button>
@@ -55,17 +55,17 @@ class ButtonsView extends React.Component<{}, {}> {
             </div>
           </div>
 
-          <div className="+flex-column">
-            <h3>{'States'}</h3>
-            <h4>{'Default'}</h4>
-            <div className="+display-flex">
+          <div className="+flex-column +push-half-bottom">
+            <h3 className="+push-half-bottom +black-text +font-weight-bold">{'States'}</h3>
+            <h4 className="+push-half-bottom +grey-text +font-weight-bold">{'Default'}</h4>
+            <div className="+display-flex +push-half-bottom">
               <Button>{'Click'}</Button>
               <Button color="grey">{'Click'}</Button>
               <Button color="white">{'Click'}</Button>
               <Button color="red">{'Click'}</Button>
             </div>
-            <h4>{'Disabled'}</h4>
-            <div className="+display-flex">
+            <h4 className="+push-half-bottom +grey-text +font-weight-bold">{'Disabled'}</h4>
+            <div className="+display-flex +push-half-bottom">
               <Button disabled={true}>{'Click'}</Button>
               <Button disabled={true} color="grey">{'Click'}</Button>
               <Button disabled={true} color="white">{'Click'}</Button>
@@ -74,10 +74,10 @@ class ButtonsView extends React.Component<{}, {}> {
           </div>
         </div>
 
-        <div className="+flex-column">
-          <h3>{'Round'}</h3>
-          <h4>{'Default'}</h4>
-          <div className="+display-flex">
+        <div className="+flex-column +push-half-bottom">
+          <h3 className="+push-half-bottom +black-text +font-weight-bold">{'Round'}</h3>
+          <h4 className="+push-half-bottom +grey-text +font-weight-bold">{'Default'}</h4>
+          <div className="+display-flex +push-half-bottom">
             <CircleButton className={'+push-right'}>
               <div className="icon-upload-cloud" />
             </CircleButton>
@@ -92,8 +92,8 @@ class ButtonsView extends React.Component<{}, {}> {
             </CircleButton>
           </div>
 
-          <h4>{'Small'}</h4>
-          <div className="+display-flex">
+          <h4 className="+push-half-bottom +grey-text +font-weight-bold">{'Small'}</h4>
+          <div className="+display-flex +push-half-bottom">
             <CircleButton className={'small +push-right'}>
               <div className="icon-upload-cloud" />
             </CircleButton>
@@ -108,8 +108,8 @@ class ButtonsView extends React.Component<{}, {}> {
             </CircleButton>
           </div>
 
-          <h4>{'Large'}</h4>
-          <div className="+display-flex">
+          <h4 className="+push-half-bottom +grey-text +font-weight-bold">{'Large'}</h4>
+          <div className="+display-flex +push-half-bottom">
             <CircleButton className={'large +push-right'}>
               <div className="icon-upload-cloud" />
             </CircleButton>
@@ -125,32 +125,34 @@ class ButtonsView extends React.Component<{}, {}> {
           </div>
         </div>
 
-        <div className="+display-flex">
-          <div className="+flex-column">
-            <h3>{'Button Groups'}</h3>
-            <h4>{'Default with Icons'}</h4>
+        <div className="+display-flex +push-half-bottom">
+          <div className="+flex-column +push-half-bottom">
+            <h3 className="+push-half-bottom +black-text +font-weight-bold">{'Button Groups'}</h3>
+            <h4 className="+push-half-bottom +grey-text +font-weight-bold">{'Default with Icons'}</h4>
             <ButtonGroup>
               <Button color="white" disabled={false}>{'Click'}<span className="icon-paperclip" /></Button>
               <Button color="white" disabled={false}>{'Click2'}<span className="icon-paperclip" /></Button>
               <Button color="white" disabled={false}>{'Click3'}<span className="icon-paperclip" /></Button>
               <Button color="white" disabled={false}>{'Click4'}<span className="icon-paperclip" /></Button>
             </ButtonGroup>
-            <h4>{'Icon'}</h4>
-            <Button color="white" disabled={false}><span className="icon-paperclip" /></Button>
-            <Button color="white" disabled={false}><span className="icon-paperclip" /></Button>
-            <Button color="white" disabled={false}><span className="icon-paperclip" /></Button>
-            <Button color="white" disabled={false}><span className="icon-paperclip" /></Button>
+            <h4 className="+push-half-bottom +grey-text +font-weight-bold">{'Icon'}</h4>
+            <ButtonGroup>
+              <Button color="white" disabled={false}><span className="icon-paperclip" /></Button>
+              <Button color="white" disabled={false}><span className="icon-paperclip" /></Button>
+              <Button color="white" disabled={false}><span className="icon-paperclip" /></Button>
+              <Button color="white" disabled={false}><span className="icon-paperclip" /></Button>
+            </ButtonGroup>
           </div>
           <div className="+flex-column">
-            <h3>{'Minimal'}</h3>
-            <h4>{'Default'}</h4>
+            <h3 className="+push-half-bottom +black-text +font-weight-bold">{'Minimal'}</h3>
+            <h4 className="+push-half-bottom +grey-text +font-weight-bold">{'Default'}</h4>
             <ButtonGroup>
               <Button color="minimal" disabled={false}>{'Click'}</Button>
               <Button color="minimal" disabled={false}>{'Click2'}</Button>
               <Button color="minimal" disabled={false}>{'Click3'}</Button>
               <Button color="minimal" disabled={false}>{'Click4'}</Button>
             </ButtonGroup>
-            <h4>{'Default with Icon'}</h4>
+            <h4 className="+push-half-bottom +grey-text +font-weight-bold">{'Default with Icon'}</h4>
             <ButtonGroup>
               <Button color="minimal" disabled={false}>{'Click'}<span className="icon-paperclip" /></Button>
               <Button color="minimal" disabled={false}>{'Click2'}<span className="icon-paperclip" /></Button>
