59830b1e4b60a407e5749ae8882dc36d216125ca
Kevin Belanger <kevinlbelanger@gmail.com>
2018-06-06 15:50:11 -0400

    disabled circle button

--- src/ts/views/buttons/circle.tsx
+++ src/ts/views/buttons/circle.tsx
@@ -6,13 +6,15 @@ const CircleButton = (props: {
   children?: any | undefined,
   className: string,
   style?: any,
-  onClick?: () => void
+  onClick?: () => void,
+  disabled?: boolean
 }) => {
   return (
     <Button {...props}
       onClick={props.onClick}
       className={classnames('circle', props.className)}
       style={props.style}
+      disabled={props.disabled}
     >
       {props.children}
     </Button>
